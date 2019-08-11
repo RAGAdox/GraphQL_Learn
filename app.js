c339674827c3466273d652b557f29b25f44043ad
RAGAdox <rishitech882@gmail.com>
2019-08-12 04:10:19 +0530

    pg1

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
-});
+app.use(
+  "/api",
+  graphqlHttp({
+    schema: null,
+    rootValue: {}
+  })
+);
 
 app.listen(3000);
